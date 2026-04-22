147de4559e2f47aad3e8394b028d39c30a780c73
gh0st4n <darskevoid@gmail.com>
2026-04-22 13:29:29 +0700

    Update

--- Python/code/02data-type.py
+++ Python/code/02data-type.py
@@ -2,5 +2,12 @@
 """String/str"""
 nama = "Gh0sT4n"
 nama_pr:str = "Miftah"
+print("=== String Data Type ===")
 print(f"Nama : {nama}, Data Type : {type(nama)}")
 print(f"Nama : {nama_pr}, Data Type : {type(nama_pr)}\n")
+
+integer = 17
+integer2:int = 18
+
+
+print("\n=== Numeric Data Type")
